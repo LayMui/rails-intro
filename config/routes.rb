518cf89617e22e52efea117251edc4c899e7d5cd
LayMui <mui_ume@hotmail.com>
2025-01-15 10:24:01 +0800

    using the path helper

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
   root to: "pages#home"
-  get "about", to: "pages#about"
+  get "home", to: "pages#home"
+  get "about", to: "pages#about", as: :laymui_about
   get "contact", to: "pages#contact"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
